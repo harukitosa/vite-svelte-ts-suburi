cef304030a1a28b7c96c854e6eccd841be7eb3ef
harukitosa <13haruki28@gmail.com>
2021-05-09 17:06:24 +0900

    first commit

---
+++ src/lib/Sample.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    interface Props {
+        name: String
+        age: Number
+    }
+
+    export let props: Props;
+</script>
+
+<main>
+    <div>
+        <h1>{props.name}</h1>
+        <h2>{props.age}</h2>
+    </div>
+</main>
+
+<style>
+
+</style>
